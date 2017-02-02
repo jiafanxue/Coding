f566b6f5e925ecb0a7ec8e49ef6f10c85bc2e42f
Tutuxsfly <Tutuxsfly@126.com>
2017-02-03 00:08:21 +0800

    Update xstl_allocator.h --version1.1

--- STL_re/library/xstl_allocator.h
+++ STL_re/library/xstl_allocator.h
@@ -23,17 +23,15 @@ namespace xstl
 	}
 
 	template <typename T>
-	inline void _deallocate(T* buffer, std::size_t n)
+	inline void _deallocate(T* buffer, std::size_t /* n */)
 	{
-		for(std::size_t i = 0; i < n; ++i) {
-			::operator delete(buffer + i);
-		}
+		::operator delete(buffer);
 	}
 
 	template <typename T1, typename T2>
 	inline void _construct(T1* p, const T2& val)
 	{
-		new ((void *)p)T1(val);
+		new ((void *)p) T1(val);
 	}
 
 	template <typename T>
@@ -55,16 +53,22 @@ namespace xstl
 		using size_type  		= std::size_t;
 		using difference_type   = std::ptrdiff_t;
 		// using propagate_on_container_move_assignment = std::true_type;
-		// using rebind 			= template <class U> struct rebind { typedef allocator<U> other;};
+
+		template <class U> struct rebind {
+			typedef allocator<U> other;
+		};
+		
 		// using is_always_equal 	= std::true_type;
 
 	public:
 		// Member functions
-		// allocator();
-		// allocator(const allocator& other);
-		// template <class U>
-		// allocator(const allocator<U>& other);
-		// ~allocator();
+		/*
+			costructor && destructor
+		*/
+		allocator() {}
+		allocator(const allocator& other) {}
+		template <class U> allocator(const allocator<U>& other) {}
+		~allocator() {}
 
 		/*
 			address : obtains the address of an object, even if operator& is overloaded
@@ -77,6 +81,9 @@ namespace xstl
 		const_pointer address(const_reference x) const
 		{
 			return static_cast<const_reference>(&x);
+			/*
+				In sgi stl only return &x;
+			*/
 		}
 
 		/*
@@ -86,21 +93,25 @@ namespace xstl
 		{
 			return _allocate(static_cast<difference_type>(n),
 				static_cast<pointer>(0));
+			/*
+				return n != 0 ? static_cast<pointer>(_Alloc::allocte(n * sizeof(T*))) : 0;
+			*/
 		}
 
-
-		// T* allocate(std::size_t n)
-		// {
-		// 	return _allocate(static_cast<difference_type>(n),
-		// 		static_cast<pointer>(0));
-		// }
+		/*
+			T* allocate(std::size_t n)
+			{
+				return _allocate(static_cast<difference_type>(n),
+					static_cast<pointer>(0));
+			}
+		*/
 
 		/*
 			deallocate : deallocates storage
 		*/
 		void deallocate(T* p, std::size_t n)
 		{
-			_deallocate(p, n);
+			_deallocate(p, n * sizeof(pointer));
 		}
 
 		/*
@@ -109,6 +120,9 @@ namespace xstl
 		size_type max_size() const
 		{
 			return static_cast<size_type>(UINT_MAX / sizeof(T));
+			/*
+				return size_t(-1) / sizeof(T);
+			*/
 		}
 
 		/*
@@ -127,7 +141,35 @@ namespace xstl
 			_destroy(p);
 		}
 	};
-}
 
+	template<>
+	class allocator<void>
+	{
+	public:
+		// Member types
+		using value_type 		= void;
+		using pointer	 		= void*;
+		using const_pointer 	= const void*;
+		using size_type  		= std::size_t;
+		using difference_type   = std::ptrdiff_t;
+		// using propagate_on_container_move_assignment = std::true_type;
+		template <class U> struct rebind {
+			typedef allocator<U> other;
+		};
+		// using is_always_equal 	= std::true_type; 
+	};
+
+	template <typename T1, typename T2>
+	bool operator==(const allocator<T1>& lhs, const allocator<T2>& rhs)
+	{
+		return true;
+	}
+
+	template <typename T1, typename T2>
+	bool operator!=(const allocator<T1>& lhs, const allocator<T2>& rhs)
+	{
+		return false;
+	}
+}
 
 #endif // LIBRARY_XSTL_ALLOCATOR_H__
