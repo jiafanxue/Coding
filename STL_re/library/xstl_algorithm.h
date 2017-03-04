ec0b9250da9b1bfac037e3fea271a5d17d13a58a
Tutuxsfly <Tutuxsfly@126.com>
2017-03-04 15:59:24 +0800

    Add xstl_algorithm -v1.0

---
+++ STL_re/library/xstl_algorithm.h
@@ -0,0 +1,18 @@
+#ifndef LIBRARY_XSTL_ALGORITHM_H__
+#define LIBRARY_XSTL_ALGORITHM_H__
+
+// Non-modifying sequence operations
+#include "xstl_algo_find.h"				// find && find_if && find_if_not
+
+
+// Modifying sequence operations
+#include "xstl_algo_copy.h"				// copy && copy_if && copy_n && copy_backward
+#include "xstl_algo_fill.h"				// fill && fill_n
+
+#include "xstl_algo_unique.h"			// unique && unique_copy
+
+// Sorting operations(on sorted ranges)
+// #include "xstl_algo_sort.h"				// is_sorted && is_srted_until && sort && 
+										// partial_sort && partial_sort_copy && stable_sort && nth_element
+
+#endif // LIBRARY_XSTL_ALGORITHM_H__
